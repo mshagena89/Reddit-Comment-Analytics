5d14a8387404f8883e44aa6ce096b4936ac6f14d
Michael Shagena <mshagena89@gmail.com>
2015-06-23 14:42:33 -0400

    add csvreader_all helper function

--- Reddit Web Scraper/csvhelpers.py
+++ Reddit Web Scraper/csvhelpers.py
@@ -12,3 +12,14 @@ def csvreader(sourceFile):
         for row in commentReader:
             commentList.append(row[0])
     return commentList 
+
+#returns all input rows from csv source file
+#sourceFile: .csv with any number of columns
+def csvreader_all(sourceFile):
+    commentList = []
+    with open(sourceFile, 'rb') as readFile:
+        commentReader = csv.reader(readFile)
+
+        for row in commentReader:
+            commentList.append(row)
+    return commentList 
