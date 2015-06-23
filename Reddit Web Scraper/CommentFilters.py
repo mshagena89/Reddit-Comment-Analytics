a76576e7e38dde83f9f44829ddf922d400e50d1d
Michael Shagena <mshagena89@gmail.com>
2015-06-23 14:07:07 -0400

    clean up and add import for csvhelper class

--- Reddit Web Scraper/CommentFilters.py
+++ Reddit Web Scraper/CommentFilters.py
@@ -3,13 +3,16 @@ import CSVhelpers as csvhelp
 
 #takes list of player name terms 
 #returns list of comments which contain player names
+
+##TODO: build logic to filter out comments only for individual player
+##find matches of player name, exclude list of non-players?
 def filterCommentsByPlayer(playerNameList, commentFile):
-    commentlist = csvreader(commentFile)
+    commentlist = csvhelp.csvreader(commentFile)
 
     for name in playerNameList:
         filteredList = [k for k in commentlist if name in k]
 
     return filteredList
 
-test = filterCommentsByPlayer(["Megatron"], 'C:\\Users\\Mike\\Documents\\GitHub\\RedditAnalytics\\Fantasy\\06.23.2015\\Parsed.csv')
+
 
